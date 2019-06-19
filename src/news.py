d7b022d26bff3cb34d7ca118bcbb7d7b4380bc0b
Davis Crawford <davis1@vt.edu>
2019-06-19 13:59:07 -0400

    configured news.py with tiingo

--- src/news.py
+++ src/news.py
@@ -1,10 +1,9 @@
-from newsapi.newsapi_client import NewsApiClient
+from tiingo import TiingoClient
 
 if __name__ == "__main__":
-    newsapi = NewsApiClient(api_key='66af3123197e43a4b55137cfddf67a2c')
-    top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                              category='business',
-                                              language='en',
-                                              country='us')
-    print(top_headlines)
-
+    config = {}
+    config['session'] = True
+    config['api_key'] = "68410637165451ba499a7ce1142e02e7da1ea0d7"
+    client = TiingoClient(config)
+    ticker_metadata = client.get_ticker_metadata("GOOGL")
+    print(ticker_metadata)
