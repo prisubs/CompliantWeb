bb250208d0116f143ec3986714f2239ddd8e27ef
rts-2 <rts2@illinois.edu>
2019-06-20 14:00:03 -0500

    got indico io working because it turns out the api key that was put into the sentiment calculator file was wildly inaccurate

--- src/CONSTANTS.py
+++ src/CONSTANTS.py
@@ -5,5 +5,5 @@ consumer_secret = ''
 access_token = ''
 access_token_secret = ''
 newsapikey = "66af3123197e43a4b55137cfddf67a2c"
-INDICOIO_KEY = "0AGGE0X9UYCDMkHxZWHojX0uBIaoHNZCQbfJO8hFx0g7nj9OJYEPJl2NzdBDdgtJ"
+INDICOIO_KEY = "026012f2d0b869994cf77bf084d58a97"
 TIINGO_KEY = 'a265fc4a1013923f970d16e7348195074e97fcb0'
--- src/sentiment_calculator.py
+++ src/sentiment_calculator.py
@@ -2,7 +2,7 @@ import indicoio
 import CONSTANTS
 import scraping_utils
 import requests
-indicoio.config.api_key = "0AGGE0X9UYCDMkHxZWHojX0uBIaoHNZCQbfJO8hFx0g7nj9OJYEPJl2NzdBDdgtJ"
+indicoio.config.api_key = "026012f2d0b869994cf77bf084d58a97"
 
 data = []
 #we may have to change this into a csv, I'm not sure yet how we are going to format our files
@@ -19,11 +19,10 @@ def get_individ_sentiment_from_ary_and_store_as_dataframe(ary):
     return data_frame
 
 #below are just some examples of different features in indicoio
-#example_of_article_sentiment = indicoio.sentiment_hq("http://www.vanityfair.com/news/2015/10/the-serious-problem-with-treating-donald-trump-seriously", url=True)
+example_of_article_sentiment = indicoio.sentiment_hq("http://www.vanityfair.com/news/2015/10/the-serious-problem-with-treating-donald-trump-seriously", url=True)
 #example_of_indicoio_keywords = indicoio.keywords("http://www.vanityfair.com/news/2015/10/the-serious-problem-with-treating-donald-trump-seriously", url=True, top_n=5)
-'''
+
 if __name__ == "__main__":
     print(indicoio.sentiment('This sentence is awful. This sentence is great!', split='sentence'))
     print("sentiment of the vanity fair article is {0}".format(example_of_article_sentiment))
-    print("keywords of the article are:\n{0}".format(keywords))
-'''
+    #print("keywords of the article are:\n{0}".format(keywords))
