1773d20cae4a04ba7e07d922acd184396f14df2b
Prianka <prianka.sub@berkeley.edu>
2019-06-24 09:48:21 -0400

    updated the load functions in utils

--- utils.py
+++ utils.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import numpy as np
-import datetime
+# import datetime
 
 def load_clean(path):
-    DESIRED = ["Ticker","Start", "End", "Open", "Close", "High", "Low", "Volume", "Average"]
+    DESIRED = ["Ticker","Date", "Open", "Close", "High", "Low", "Volume", "Average"]
     df = pd.read_csv(path)
-    df["Ticker"] = df["Ticker"].apply(lambda t: t[9:])
-    df = fix_dates(df)
     df = df[DESIRED]
     return df
 
+'''
 def fix_dates(df):
     df["Date"] = df["Date"].apply(lambda x: pd.to_datetime(x, infer_datetime_format=True))
     df["Start"] = df["Date"].apply(str)
@@ -21,15 +20,15 @@ def fix_dates(df):
     df["Start"] = df["Start"].apply(remove_time)
     df["End"] = df["End"].apply(remove_time)
     return df
-
+'''
 
 # returns all company dataframes in the following order
 # copy and paste the example usage command below
 # AAPL, AMZN, BABA, CRM, FB, GOOGL, MSFT, NFLX, TSLA, WMT = load_training_data()
 # gives all ten dataframes
 def load_training_data():
-    fn = ["data/original/AAPL.csv", "data/original/AMZN.csv", "data/original/BABA.csv", 
-    "data/original/CRM.csv", "data/original/FB.csv", "data/original/GOOGL.csv", "data/original/MSFT.csv", 
-    "data/original/NFLX.csv", "data/original/TSLA.csv", "data/original/WMT.csv"]
+    fn = ["data/formatted/AAPL.csv", "data/formatted/AMZN.csv", "data/formatted/BABA.csv", 
+    "data/formatted/CRM.csv", "data/formatted/FB.csv", "data/formatted/GOOGL.csv", "data/formatted/MSFT.csv", 
+    "data/formatted/NFLX.csv", "data/formatted/TSLA.csv", "data/formatted/WMT.csv"]
 
     return load_clean(fn[0]), load_clean(fn[1]), load_clean(fn[2]), load_clean(fn[3]), load_clean(fn[4]), load_clean(fn[5]), load_clean(fn[6]), load_clean(fn[7]), load_clean(fn[8]), load_clean(fn[9])
