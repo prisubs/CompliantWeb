246b786a8bd49d9a2c2de4726415d70edc7191a6
rts-2 <rts2@illinois.edu>
2019-07-19 10:38:21 -0400

    adding a lot more cards, grids, and elements to the predict page

--- web/src/components/Predict.component.js
+++ web/src/components/Predict.component.js
@@ -65,6 +65,104 @@ export default class Predict extends Component {
             <StatsCard layout={1} movement={-1} total="621" label="Products" />
           </Grid.Col>
         </Grid.Row>
+
+        <Grid.Row cards={true} className="pies-and-big-numbers">
+          <Grid.Col sm={6}>
+            <Card>
+              <Card.Header>
+                <Card.Title>Article Ratio </Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <C3Chart
+                  style={{ height: '12rem' }}
+                  data={{
+                    columns: [
+                      // each columns data
+                      ['data1', 63],
+                      ['data2', 37]
+                    ],
+                    type: 'donut', // default type of chart
+                    colors: {
+                      data1: colors['green'],
+                      data2: colors['red']
+                    },
+                    names: {
+                      // name of each serie
+                      data1: 'Positive Articles',
+                      data2: 'Negative Articles'
+                    }
+                  }}
+                  legend={{
+                    show: false //hide legend
+                  }}
+                  padding={{
+                    bottom: 0,
+                    top: 0
+                  }}
+                />
+              </Card.Body>
+            </Card>
+          </Grid.Col>
+          <Grid.Col sm={6}>
+            <Card>
+              <Card.Header>
+                <Card.Title>Sentiment Ratio</Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <C3Chart
+                  style={{ height: '12rem' }}
+                  data={{
+                    columns: [
+                      // each columns data
+                      ['data1', 63],
+                      ['data2', 44],
+                      ['data3', 12],
+                      ['data4', 14]
+                    ],
+                    type: 'pie', // default type of chart
+                    colors: {
+                      data1: colors['blue-darker'],
+                      data2: colors['blue'],
+                      data3: colors['blue-light'],
+                      data4: colors['blue-lighter']
+                    },
+                    names: {
+                      // name of each serie
+                      data1: 'A',
+                      data2: 'B',
+                      data3: 'C',
+                      data4: 'D'
+                    }
+                  }}
+                  legend={{
+                    show: false //hide legend
+                  }}
+                  padding={{
+                    bottom: 0,
+                    top: 0
+                  }}
+                />
+              </Card.Body>
+            </Card>
+          </Grid.Col>
+
+          <Grid.Col sm={6} className="big-numbers">
+            <ProgressCard
+              header="New feedback"
+              content="62"
+              progressColor="red"
+              progressWidth={28}
+            />
+          </Grid.Col>
+          <Grid.Col sm={6} className="big-numbers">
+            <ProgressCard
+              header="Today profit"
+              content="$652"
+              progressColor="green"
+              progressWidth={84}
+            />
+          </Grid.Col>
+        </Grid.Row>
       </div>
     )
   }
